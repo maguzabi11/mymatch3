2d51e9bd9f1bd38c2cdad40417a40f83c198b237
maguz <maguzabi11@gmail.com>
2019-06-03 10:44:17 +0900

    첫 등록

---
+++ Assets/Tests/Editor/TestGamePanel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using Match3;
+
+namespace Tests
+{
+    // fixture 자동?
+    public class TestGamePanel
+    {
+        GamePanel gp;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Use the Assert class to test conditions
+            gp = new GamePanel(4, 5);
+        }
+
+        [Test]
+        public void TestGamePanelSize()
+        {
+            Assert.AreEqual( gp.tiles.Length, 20 );
+        }
+
+        [Test]
+        public void TestCreateTiles()
+        {
+            bool b = gp.CreateTiles();
+            Assert.That(b == true);
+            gp.OutputTiles();
+        }
+
+        [Test]
+        public void TestMatchTile()
+        {
+            gp.CreateSpecificTilesforTest();
+            
+            Assert.IsTrue(gp.IsMatch3Tile(0, 2));
+        }
+
+        //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        //// `yield return null;` to skip a frame.
+        //[UnityTest]
+        //public IEnumerator TestGamePanelWithEnumeratorPasses()
+        //{
+        //    // Use the Assert class to test conditions.
+        //    // Use yield to skip a frame.
+        //    yield return null;
+        //}
+    }
+}
---
+++ Assets/Tests/mbTestObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Match3;
+
+public class mbTestObject : MonoBehaviour
+{
+    GamePanel gp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gp = new GamePanel(4, 5);
+        gp.CreateTiles();
+        gp.OutputTiles();
+
+        int numMatch = gp.FindAllMatches();
+        Debug.LogFormat( $"Matches:{numMatch}");
+
+        gp.OutputMatches();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
